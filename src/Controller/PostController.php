d8d8ac943145f7971e5acf769926bfbc705e1bcf
Karol <karol.123@vp.pl>
2023-12-28 10:25:44 +0100

    Adding a GET API to retrieve all posts from the database

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,26 +2,40 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\PostRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
+#[ApiResource(operations: [
+    new GetCollection()
+], normalizationContext: ['groups' => ['post']],
+    paginationEnabled: false)]
 #[ORM\Entity(repositoryClass: PostRepository::class)]
 class Post
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['post'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['post'])]
     private ?string $body = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['post'])]
     private ?string $title = null;
 
-    #[ORM\ManyToOne(inversedBy: 'posts')]
+    #[ORM\ManyToOne(fetch: 'EAGER', inversedBy: 'posts')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['post'])]
     private ?User $author = null;
 
     public function getId(): ?int
